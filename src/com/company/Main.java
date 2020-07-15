d4e4c00a534bdbc67c023be0c071432c7e97c492
Valeria-bit <valerka_sk02@mail.ru>
2020-07-15 21:58:27 +0300

    добавилась табличка с номерами клеток.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,13 @@ public class Main {
     public static void main(String[] args) {
         GameField field = new GameField();
         System.out.println("Игра началась...");
-        // напечатать номера клеток
+        System.out.println("*-----------*\n"+
+                           "| 1 | 2 | 3 |\n"+
+                           "|-----------|\n"+
+                           "| 4 | 5 | 6 |\n"+
+                           "|-----------|\n"+
+                           "| 7 | 8 | 9 |\n"+
+                           "-------------\n");
         field.print();
         Symbol player = Symbol.krestik;
         while (!field.gameOver()) {
